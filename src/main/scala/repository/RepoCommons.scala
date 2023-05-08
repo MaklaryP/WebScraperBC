cbbc0461d9f4202f0dfbd371af0e6db66756d854
maklapet <peter3maklary@gmail.com>
2023-05-08 11:49:41 +0200

    Add feed date column to csv and header

--- src/main/scala/repository/RepoCommons.scala
+++ src/main/scala/repository/RepoCommons.scala
@@ -6,19 +6,25 @@ import org.apache.commons.csv.{CSVFormat, CSVPrinter}
 
 import java.io.FileWriter
 import java.nio.file.Path
+import java.time.LocalDateTime
 
 object RepoCommons {
 
 
-  def saveDtosToCsv(dtos: Seq[RepoDTO], path: Path): Unit = {
+
+  def saveDtosToCsv(dtos: Seq[RepoDTO], path: Path, header: Option[Seq[String]] = None): Unit = {
 
     CLogger.getLogger.log(s"Writing to a file: ${path.toString} -- Start")
 
     val writer = new FileWriter(path.toFile, true)
     val printer = new CSVPrinter(writer, CSVFormat.DEFAULT)
 
+    def printToCsv(s: Seq[String]): Unit = printer.printRecord(s: _*)
+
+    header.foreach(printToCsv)
+
     dtos.foreach { dto =>
-      printer.printRecord(RepoCommons.repoDtoToSeqMapper(dto): _*)
+      printToCsv(RepoCommons.repoDtoToSeqMapper(dto, LocalDateTime.now()))
     }
 
     printer.flush()
@@ -29,11 +35,12 @@ object RepoCommons {
   }
 
 
-  def repoDtoToSeqMapper(repoDTO: RepoDTO): Seq[String] = {
+  def repoDtoToSeqMapper(repoDTO: RepoDTO, feedTimestamp: LocalDateTime): Seq[String] = {
     Seq(
       repoDTO.state,
       repoDTO.url,
       repoDTO.error_msg,
+      feedTimestamp.toString,
       repoDTO.title,
       repoDTO.authors.mkString(", "),
       repoDTO.intro_section,
@@ -43,6 +50,22 @@ object RepoCommons {
       repoDTO.child_urls.mkString(", ")
     )
 
+  }
+
+  def getCsvHeader(): Seq[String] = {
+    Seq(
+      "state",
+      "url",
+      "error_msg",
+      "feed_timestamp",
+      "title",
+      "authors",
+      "intro_section",
+      "article_text",
+      "publish_date",
+      "lst_upd_dt",
+      "child_urls"
+    )
   }
 
 }
